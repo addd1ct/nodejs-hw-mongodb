081e1dcd9fac34759ed68ca827b2f90c2082b4b5
polly <polifibliss@gmail.com>
2025-06-14 16:11:12 +0300

    edit

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 export async function initMongoConnection() {
 
-  const uri = 'mongodb+srv://polifibliss:9YP0D8hz7MQA4U9D@cluster0.qwbuzs9.mongodb.net/students?retryWrites=true&w=majority';
+  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 
   await mongoose.connect(uri);
   console.log('Mongo connection successfully established!');
