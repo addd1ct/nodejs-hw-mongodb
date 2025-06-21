e07b2fdc19f3982bdf259805987864af925b3a50
polly <polifibliss@gmail.com>
2025-06-21 14:18:50 +0300

    edit

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,11 +1,22 @@
-import createHttpError from 'http-errors';
+// import createHttpError from 'http-errors';
 
-export const validateBody = (schema) => {
-  return (req, res, next) => {
-    const { error } = schema.validate(req.body, { abortEarly: false });
-    if (error) {
-      return next(createHttpError(400, error.message));
-    }
-    next();
-  };
-};
+// export const validateBody = (schema) => {
+//   return (req, res, next) => {
+//     const { error } = schema.validate(req.body, { abortEarly: false });
+//     if (error) {
+//       return next(createHttpError(400, error.message));
+//     }
+//     next();
+//   };
+// };
+
+export function errorHandler(err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message || 'Internal Server Error';
+  console.error(err);
+  res.status(status).json({
+    status,
+    message,
+    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
+  });
+}
