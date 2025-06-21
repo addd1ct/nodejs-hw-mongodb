a340f03077e551d1bbbabbfbed0c491df704ee82
polly <polifibliss@gmail.com>
2025-06-21 21:17:39 +0300

    edit

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,18 +1,8 @@
-// export function errorHandler(err, req, res, next) {
-//     const { status = 500, message = 'Internal Server Error' } = err;
-//     res.status(status).json({
-//       status,
-//       message,
-//     });
-//   }
-  
 export function errorHandler(err, req, res, next) {
-  const status = err.status || 500;
-  const message = err.message || 'Internal Server Error';
-  console.error(err);
-  res.status(status).json({
-    status,
-    message,
-    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
-  });
-}
+    const { status = 500, message = 'Internal Server Error' } = err;
+    res.status(status).json({
+      status,
+      message,
+    });
+  }
+  
