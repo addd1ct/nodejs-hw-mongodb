f65196858492b9d2d1b55ae03139cabcde2f82cc
polly <polifibliss@gmail.com>
2025-06-24 01:10:00 +0300

    edit

--- src/routers/apiDocs.js
+++ src/routers/apiDocs.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -8,11 +9,10 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const swaggerDocument = path.join(__dirname, '../../docs/swagger.json');
+const swaggerPath = path.join(__dirname, '../../docs/swagger.json');
+const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf-8'));
 
 router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(null, {
-  swaggerUrl: '/swagger.json',
-}));
+router.get('/', swaggerUi.setup(swaggerDocument));
 
 export default router;
